ed894daddea1611d8f7511d8ecc8bf426fab9285
Ruarke Bullock <Ruarke_Bullock@outlook.com>
2024-09-30 11:00:42 -0400

    q

--- McDonalds Online App/Form1.cs
+++ McDonalds Online App/Form1.cs
@@ -38,33 +38,51 @@ namespace McDonalds_Online_App
 
         private void calculateButton_Click(object sender, EventArgs e)
         {
-            //inputing sounds
-            SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.Cash_Register_Cha_Ching_SoundBible_com_184076484);
-            soundPlayer.Play();
+            try
+            {
+                //inputing sounds
+                SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.Cash_Register_Cha_Ching_SoundBible_com_184076484);
+                soundPlayer.Play();
 
-            // inputs
-            burgers = (int)Convert.ToDouble(burgersOutput.Text);
-            fries = (int)Convert.ToDouble(friesOutput.Text);
-            drinks = (int)Convert.ToDouble(drinksOutput.Text);
-            //calculating totals
-            subTotal = burgers * 4.50 + fries * 2.13 + drinks * 1.45;
-            taxAmount = subTotal * 0.13;
-            total = subTotal + taxAmount;
+                // inputs
+                burgers = (int)Convert.ToDouble(burgersOutput.Text);
+                fries = (int)Convert.ToDouble(friesOutput.Text);
+                drinks = (int)Convert.ToDouble(drinksOutput.Text);
+                //calculating totals
+                subTotal = burgers * 4.50 + fries * 2.13 + drinks * 1.45;
+                taxAmount = subTotal * 0.13;
+                total = subTotal + taxAmount;
 
-            subTotalOutput.Text = $"{subTotal.ToString("C")}";
-            taxOutput.Text = $"{taxAmount.ToString("C")}";
-            totalOutput.Text = $"{total.ToString("C")}";
+                subTotalOutput.Text = $"{subTotal.ToString("C")}";
+                taxOutput.Text = $"{taxAmount.ToString("C")}";
+                totalOutput.Text = $"{total.ToString("C")}";
+            }
+            catch
+            {
+                subTotalOutput.Text = $"ERROR";
+                taxOutput.Text = $"ERROR";
+                totalOutput.Text = $"ERROR";
+            }
         }
+
 
         private void calculateChangeButton_Click(object sender, EventArgs e)
         {
-            //inputing sounds
-            SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.Cash_Register_Cha_Ching_SoundBible_com_184076484);
-            soundPlayer.Play();
-            cashTendered = Convert.ToDouble(cashTenderdOutput.Text);
+            try
+            {
+                //inputing sounds
+                SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.Cash_Register_Cha_Ching_SoundBible_com_184076484);
+                soundPlayer.Play();
+                cashTendered = Convert.ToDouble(cashTenderdOutput.Text);
 
-            change = cashTendered - total;
-            changeOutput.Text = $"{change.ToString("C")}";
+                change = cashTendered - total;
+                changeOutput.Text = $"{change.ToString("C")}";
+            }
+            catch
+            {
+                change = cashTendered - total;
+                changeOutput.Text = $"ERROR";
+            }
         }
 
         private void printReceiptButton_Click(object sender, EventArgs e)
@@ -73,38 +91,41 @@ namespace McDonalds_Online_App
             SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.Sharp_Punch_SoundBible_com_1947392621__1_);
             soundPlayer.Play();
 
-            //inputing my data
-            receiptLabel.Text += $"\nOrderNumber #{order++}";
-            Refresh();
-            Thread.Sleep(1000);
-            receiptLabel.Text += $"\nseptember 25th, 2024";
-            Refresh();
-            Thread.Sleep(1000);
-            receiptLabel.Text += $"\nBurgers  x{burgers}                    $4.50";
-            Refresh();
-            Thread.Sleep(1000);
-            receiptLabel.Text += $"\nFries   x{fries}                       $2.13";
-            Refresh(); 
-            Thread.Sleep(1000);
-            receiptLabel.Text += $"\nDrinks  x{drinks}                      $1.45";
-            Refresh();
-            Thread.Sleep(1000);
-            receiptLabel.Text += $"\n\nsubtotal         {subTotal.ToString    ("C")}";
-            Refresh();
-            Thread.Sleep(1000);
-            receiptLabel.Text += $"\ntax              {taxAmount.ToString   ("C")}";
-            Refresh();
-            Thread.Sleep(1000);
-            receiptLabel.Text += $"\ntotal            {total.ToString       ("C")}";
-            Refresh();
-            Thread.Sleep(1000);
-            receiptLabel.Text += $"\ntendered         {cashTendered.ToString("C")}";
-            Refresh();
-            Thread.Sleep(1000);
-            receiptLabel.Text += $"\nchange           {change.ToString      ("C")}";
-            Refresh();
-            Thread.Sleep(1000);
-            receiptLabel.Text += $"\n\n Thanks For Coming To McDonalds!! Have A Great Day!";
+                        
+                //inputing my data
+                receiptLabel.Text += $"\nOrderNumber #{order++}";
+                Refresh();
+                Thread.Sleep(1000);
+                receiptLabel.Text += $"\nseptember 25th, 2024";
+                Refresh();
+                Thread.Sleep(1000);
+                receiptLabel.Text += $"\nBurgers  x{burgers}                    $4.50";
+                Refresh();
+                Thread.Sleep(1000);
+                receiptLabel.Text += $"\nFries   x{fries}                       $2.13";
+                Refresh();
+                Thread.Sleep(1000);
+                receiptLabel.Text += $"\nDrinks  x{drinks}                      $1.45";
+                Refresh();
+                Thread.Sleep(1000);
+                receiptLabel.Text += $"\n\nsubtotal         {subTotal.ToString("C")}";
+                Refresh();
+                Thread.Sleep(1000);
+                receiptLabel.Text += $"\ntax              {taxAmount.ToString("C")}";
+                Refresh();
+                Thread.Sleep(1000);
+                receiptLabel.Text += $"\ntotal            {total.ToString("C")}";
+                Refresh();
+                Thread.Sleep(1000);
+                receiptLabel.Text += $"\ntendered         {cashTendered.ToString("C")}";
+                Refresh();
+                Thread.Sleep(1000);
+                receiptLabel.Text += $"\nchange           {change.ToString("C")}";
+                Refresh();
+                Thread.Sleep(1000);
+                receiptLabel.Text += $"\n\n Thanks For Coming To McDonalds!! Have A Great Day!";
+           
+
         }
 
         private void newOrderButton_Click(object sender, EventArgs e)
